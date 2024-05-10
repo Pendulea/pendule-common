577cd8057d0f8409ed2f1e38bde17a24f220b6d6
fantasim <siuol@gmx.com>
2024-05-10 05:15:29 +0300

    +++

--- file.go
+++ file.go
@@ -245,3 +245,17 @@ func (f file) ListenPairJSONFileChange(pairsPath string, callback func(path stri
 	// Block forever
 	select {}
 }
+
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
--- rpc-client-requests.go
+++ rpc-client-requests.go
@@ -1,5 +1,7 @@
 package pcommon
 
+import "fmt"
+
 type parserRPCRequests struct{}
 
 func (rpc *parserRPCRequests) FetchCandleList(parserRPCClient *RPCClient, settings GetCandlesRequest) (TickTimeArray, error) {
@@ -37,4 +39,67 @@ func (rpc *parserRPCRequests) FetchAvailablePairSetList(parserRPCClient *RPCClie
 	setList := GetSetListsResponse{}
 	Format.DecodeMapIntoStruct(res.Data, &setList)
 	return setList.SetList, nil
+}
+
+func (rpc *parserRPCRequests) IsDateParsed(parserRPCClient *RPCClient, settings IsDateParsedRequest) (bool, error) {
+	if err := parserRPCClient.CheckConnectedError(); err != nil {
+		return false, err
+	}
+
+	reqMap, err := Format.EncodeStructIntoMap(&settings)
+	if err != nil {
+		return false, err
+	}
+
+	res, err := parserRPCClient.Request("IsDateParsed", reqMap)
+	if err != nil {
+		return false, err
+	}
+
+	exist := IsDateParsedResponse{}
+	Format.DecodeMapIntoStruct(res.Data, &exist)
+	return exist.Exist, nil
+}
+
+func (rpc *parserRPCRequests) AddTimeframe(parserRPCClient *RPCClient, settings AddTimeFrameRequest) error {
+	if err := parserRPCClient.CheckConnectedError(); err != nil {
+		return err
+	}
+
+	reqMap, err := Format.EncodeStructIntoMap(&settings)
+	if err != nil {
+		return err
+	}
+
+	res, err := parserRPCClient.Request("AddTimeFrame", reqMap)
+	if err != nil {
+		return err
+	}
+
+	resp := AddTimeFrameResponse{}
+	Format.DecodeMapIntoStruct(res.Data, &resp)
+	if resp.Scheduled {
+		return nil
+	}
+	return fmt.Errorf("failed to schedule")
+}
+
+func (rpc *parserRPCRequests) RemoveTimeframe(parserRPCClient *RPCClient, settings RemoveTimeFrameRequest) (int, error) {
+	if err := parserRPCClient.CheckConnectedError(); err != nil {
+		return 0, err
+	}
+
+	reqMap, err := Format.EncodeStructIntoMap(&settings)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := parserRPCClient.Request("RemoveTimeframe", reqMap)
+	if err != nil {
+		return 0, err
+	}
+
+	resp := RemoveTimeFrameResponse{}
+	Format.DecodeMapIntoStruct(res.Data, &resp)
+	return resp.Count, nil
 }

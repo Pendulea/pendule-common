2980d76372613d587130bfbd102e5bb7f529b397
fantasim <siuol@gmx.com>
2024-07-02 11:38:50 +0300

    ++

--- set-json.go
+++ set-json.go
@@ -9,6 +9,7 @@ type SetJSON struct {
 	Settings SetSettings `json:"settings"`
 	Size     int64       `json:"size"`
 	Assets   []AssetJSON `json:"assets"`
+	Type     SetType     `json:"type"`
 }
 
 func (s SetJSON) PrintJSON() {

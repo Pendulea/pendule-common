a027df92c4520766bffad6791841385c5a84cf13
fantasim <siuol@gmx.com>
2024-07-03 10:21:50 +0300

    ++

--- asset-type.go
+++ asset-type.go
@@ -105,6 +105,7 @@ type AssetStateConfig struct {
 	ID       AssetType
 	DataType DataType
 
+	DefaultDecimals             int8
 	RequiredDependencyDataTypes []DataType
 	RequiredArgumentTypes       []reflect.Type
 	Label                       string
@@ -116,105 +117,106 @@ type AvailableAssets map[AssetType]AssetStateConfig
 var DEFAULT_ASSETS = AvailableAssets{
 	// Binance spot trades
 	Asset.SPOT_PRICE: {
-		Asset.SPOT_PRICE, UNIT, nil, nil,
-		"Spot Price (Binance)", "The current price at which an asset is bought or sold in the spot market on Binance.",
+		Asset.SPOT_PRICE, UNIT, -1, nil, nil,
+		"Spot Price", "The current price at which an asset is bought or sold in the spot market on Binance.",
 	},
 	Asset.SPOT_VOLUME: {
-		Asset.SPOT_VOLUME, QUANTITY, nil, nil,
-		"Spot Volume (Binance)", "The total amount of an asset traded in the spot market on Binance.",
+		Asset.SPOT_VOLUME, QUANTITY, -1, nil, nil,
+		"Spot Volume", "The total amount of an asset traded in the spot market on Binance.",
 	},
 
 	// Binance order book depth
 	Asset.BOOK_DEPTH_P1: {
-		Asset.BOOK_DEPTH_P1, UNIT, nil, nil,
-		"Liquidity +1% Price (Binance)", "Available liquidity at a price level 1% above the current market price on Binance.",
+		Asset.BOOK_DEPTH_P1, UNIT, -1, nil, nil,
+		"Liquidity +1% Price", "Available liquidity at a price level 1% above the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_P2: {
-		Asset.BOOK_DEPTH_P2, UNIT, nil, nil,
-		"Liquidity +2% Price (Binance)", "Available liquidity at a price level 2% above the current market price on Binance.",
+		Asset.BOOK_DEPTH_P2, UNIT, -1, nil, nil,
+		"Liquidity +2% Price", "Available liquidity at a price level 2% above the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_P3: {
-		Asset.BOOK_DEPTH_P3, UNIT, nil, nil,
-		"Liquidity +3% Price (Binance)", "Available liquidity at a price level 3% above the current market price on Binance.",
+		Asset.BOOK_DEPTH_P3, UNIT, -1, nil, nil,
+		"Liquidity +3% Price", "Available liquidity at a price level 3% above the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_P4: {
-		Asset.BOOK_DEPTH_P4, UNIT, nil, nil,
-		"Liquidity +4% Price (Binance)", "Available liquidity at a price level 4% above the current market price on Binance.",
+		Asset.BOOK_DEPTH_P4, UNIT, -1, nil, nil,
+		"Liquidity +4% Price", "Available liquidity at a price level 4% above the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_P5: {
-		Asset.BOOK_DEPTH_P5, UNIT, nil, nil,
-		"Liquidity +5% Price (Binance)", "Available liquidity at a price level 5% above the current market price on Binance.",
+		Asset.BOOK_DEPTH_P5, UNIT, -1, nil, nil,
+		"Liquidity +5% Price", "Available liquidity at a price level 5% above the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_M1: {
-		Asset.BOOK_DEPTH_M1, UNIT, nil, nil,
-		"Liquidity -1% Price (Binance)", "Available liquidity at a price level 1% below the current market price on Binance.",
+		Asset.BOOK_DEPTH_M1, UNIT, -1, nil, nil,
+		"Liquidity -1% Price", "Available liquidity at a price level 1% below the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_M2: {
-		Asset.BOOK_DEPTH_M2, UNIT, nil, nil,
-		"Liquidity -2% Price (Binance)", "Available liquidity at a price level 2% below the current market price on Binance.",
+		Asset.BOOK_DEPTH_M2, UNIT, -1, nil, nil,
+		"Liquidity -2% Price", "Available liquidity at a price level 2% below the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_M3: {
-		Asset.BOOK_DEPTH_M3, UNIT, nil, nil,
-		"Liquidity -3% Price (Binance)", "Available liquidity at a price level 3% below the current market price on Binance.",
+		Asset.BOOK_DEPTH_M3, UNIT, -1, nil, nil,
+		"Liquidity -3% Price", "Available liquidity at a price level 3% below the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_M4: {
-		Asset.BOOK_DEPTH_M4, UNIT, nil, nil,
-		"Liquidity -4% Price (Binance)", "Available liquidity at a price level 4% below the current market price on Binance.",
+		Asset.BOOK_DEPTH_M4, UNIT, -1, nil, nil,
+		"Liquidity -4% Price", "Available liquidity at a price level 4% below the current market price on Binance.",
 	},
 	Asset.BOOK_DEPTH_M5: {
-		Asset.BOOK_DEPTH_M5, UNIT, nil, nil,
-		"Liquidity -5% Price (Binance)", "Available liquidity at a price level 5% below the current market price on Binance.",
+		Asset.BOOK_DEPTH_M5, UNIT, -1, nil, nil,
+		"Liquidity -5% Price", "Available liquidity at a price level 5% below the current market price on Binance.",
 	},
 
 	// Metrics
 	Asset.METRIC_SUM_OPEN_INTEREST: {
-		Asset.METRIC_SUM_OPEN_INTEREST, UNIT, nil, nil,
-		"Open Interest", "The total number of outstanding derivative contracts, such as options or futures, that have not been settled.",
+		Asset.METRIC_SUM_OPEN_INTEREST, UNIT, -1, nil, nil,
+		"Open Interest", "The total number of outstanding derivative contracts, such as options or futures, that have not been settled on Binance.",
 	},
 
 	Asset.METRIC_COUNT_TOP_TRADER_LONG_SHORT_RATIO: {
-		Asset.METRIC_COUNT_TOP_TRADER_LONG_SHORT_RATIO, UNIT, nil, nil,
-		"Taker Long/Short Ratio (Binance)", "The ratio of long to short positions taken by top traders on Binance.",
+		Asset.METRIC_COUNT_TOP_TRADER_LONG_SHORT_RATIO, UNIT, 4, nil, nil,
+		"Taker Long/Short Ratio", "The ratio of long to short positions taken by top traders on Binance.",
 	},
 	Asset.METRIC_SUM_TOP_TRADER_LONG_SHORT_RATIO: {
-		Asset.METRIC_SUM_TOP_TRADER_LONG_SHORT_RATIO, UNIT, nil, nil,
-		"Top Trader Long/Short Ratio (Binance)", "The ratio of the sum of long to short positions taken by top traders on Binance.",
+		Asset.METRIC_SUM_TOP_TRADER_LONG_SHORT_RATIO, UNIT, 4, nil, nil,
+		"Top Trader Long/Short Ratio", "The ratio of the sum of long to short positions taken by top traders on Binance.",
 	},
 	Asset.METRIC_COUNT_LONG_SHORT_RATIO: {
-		Asset.METRIC_COUNT_LONG_SHORT_RATIO, UNIT, nil, nil,
-		"Long/Short Ratio (Binance)", "The overall ratio of long to short positions taken by all traders on Binance.",
+		Asset.METRIC_COUNT_LONG_SHORT_RATIO, UNIT, 4, nil, nil,
+		"Long/Short Ratio", "The overall ratio of long to short positions taken by all traders on Binance.",
 	},
 	Asset.METRIC_SUM_TAKER_LONG_SHORT_VOL_RATIO: {
-		Asset.METRIC_SUM_TAKER_LONG_SHORT_VOL_RATIO, UNIT, nil, nil,
-		"Taker Long/Short Volume Ratio (Binance)", "The ratio of long to short volumes taken by top traders on Binance.",
+		Asset.METRIC_SUM_TAKER_LONG_SHORT_VOL_RATIO, UNIT, 4, nil, nil,
+		"Taker Long/Short Volume Ratio", "The ratio of long to short volumes taken by top traders on Binance.",
 	},
 
 	// Supply
 	Asset.CIRCULATING_SUPPLY: {
-		Asset.CIRCULATING_SUPPLY, UNIT, nil, nil,
-		"Circulating Supply", "The total number of tokens that are currently available in the market and in circulation.",
+		Asset.CIRCULATING_SUPPLY, UNIT, -1, nil, nil,
+		"Circulating Supply", "The total number of tokens that are currently available in circulation.",
 	},
 
 	// Binance futures
 	Asset.FUTURES_PRICE: {
-		Asset.FUTURES_PRICE, UNIT, nil, nil,
-		"Futures Price (Binance)", "The current price at which a futures contract is trading on Binance.",
+		Asset.FUTURES_PRICE, UNIT, -1, nil, nil,
+		"Futures Price", "The current price at which a futures contract is trading on Binance.",
 	},
 	Asset.FUTURES_VOLUME: {
-		Asset.FUTURES_VOLUME, QUANTITY, nil, nil,
-		"Futures Volume (Binance)", "The total amount of futures contracts traded on Binance.",
+		Asset.FUTURES_VOLUME, QUANTITY, -1, nil, nil,
+		"Futures Volume", "The total amount of futures contracts traded on Binance.",
 	},
 
 	// Technical Indicators
 	Asset.RSI: {
-		Asset.RSI, POINT, []DataType{UNIT}, []reflect.Type{reflect.TypeOf(int64(0))},
+		Asset.RSI, POINT, 2, []DataType{UNIT}, []reflect.Type{reflect.TypeOf(int64(0))},
 		"Relative Strength Index (RSI)", "A momentum oscillator that measures the speed and change of price movements, indicating overbought or oversold conditions.",
 	},
 }
 
 type AvailableAssetJSON struct {
-	AssetType AssetType `json:"asset_type"`
-	DataType  DataType  `json:"data_type"`
+	AssetType       AssetType `json:"asset_type"`
+	DataType        DataType  `json:"data_type"`
+	DefaultDecimals int8      `json:"default_decimals"`
 
 	Dependencies        []DataType   `json:"dependencies"`
 	ArgumentTypes       []string     `json:"argument_types"`
@@ -236,6 +238,7 @@ func (aa AvailableAssets) JSON() []AvailableAssetJSON {
 		ret = append(ret, AvailableAssetJSON{
 			AssetType:           v.ID,
 			DataType:            v.DataType,
+			DefaultDecimals:     v.DefaultDecimals,
 			DataTypeName:        v.DataType.String(),
 			DataTypeColor:       v.DataType.Color(),
 			DataTypeColumns:     v.DataType.Columns(),

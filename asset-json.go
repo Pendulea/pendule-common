7b6c335101d49cf8202ba20eef1778f3eae5ed0d
fantasim <siuol@gmx.com>
2024-07-05 06:49:15 +0300

    oub

--- asset-json.go
+++ asset-json.go
@@ -8,13 +8,13 @@ type Consistency struct {
 }
 
 type AssetJSON struct {
-	AddressString              AssetAddress       `json:"address_string"`
-	Address                    AssetAddressParsed `json:"address"`
-	ConsistencyMaxLookbackDays int                `json:"consistency_max_lookback_days"`
-	Consistencies              []Consistency      `json:"consistencies"`
-	DataType                   DataType           `json:"data_type"`
-	Decimals                   int8               `json:"decimals"`
-	MinDataDate                string             `json:"min_data_date"`
+	AddressString              AssetAddress           `json:"address_string"`
+	Address                    AssetAddressParsedJSON `json:"address"`
+	ConsistencyMaxLookbackDays int                    `json:"consistency_max_lookback_days"`
+	Consistencies              []Consistency          `json:"consistencies"`
+	DataType                   DataType               `json:"data_type"`
+	Decimals                   int8                   `json:"decimals"`
+	MinDataDate                string                 `json:"min_data_date"`
 }
 
 func (a AssetJSON) FindConsistencyByTimeframe(timeframe time.Duration) *Consistency {
